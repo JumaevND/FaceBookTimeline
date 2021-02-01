874fd8702246069311461e5088c830d07ba97859
NuriddinJumaev <nuriddin.jumaev98>
2021-02-01 11:59:24 +0500

    Initial Commit

---
+++ FaceBookTimeline/FaceBookTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceBookTimelineApp.swift
+//  FaceBookTimeline
+//
+//  Created by Nuriddin Jumaev on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceBookTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
